bc8fb35a6941f67eaf5b3eebc09daa12f89f7755
Oscar <ommalor@gmail.com>
2019-01-23 14:00:37 -0500

    test

--- backend/index.babel.js
+++ backend/index.babel.js
@@ -34,7 +34,7 @@ app.set('trust proxy', 1);
 
 app.use((0, _cors2.default)({
     // origin: ['http://localhost:4200'], //the port my react app is running on.
-    origin: ['http://ec2-3-85-90-9.compute-1.amazonaws.com:3000'],
+    origin: ['https://getcars.000webhostapp.com'],
     credentials: true
 }));
 
